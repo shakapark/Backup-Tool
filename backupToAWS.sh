ca283f969ee4e118cb769b5d2f87135f9fb02460
Shakapark <shakapark1994@gmail.com>
2022-02-21 16:40:50 +0100

    Report modif of postgres backup

--- backupToAWS.sh
+++ backupToAWS.sh
@@ -223,24 +223,20 @@ function backupPostgresToBucket() {
 }
 
 function backupAllPostgresToBucket() {
-  echo "Starting Backup All Postgres"
+  set -e
 
-  echo "Remove old folder"
-  DATE=$(date -d "$RETENTION days ago" +"%d-%m-%Y")
-  aws --endpoint-url $S3_DESTINATION_HOST s3 rm --recursive s3://$S3_DESTINATION_BUCKET/postgres-$DATE
+  echo "Starting Backup All Postgres"
 
   DATE=$(date +"%d-%m-%Y")
   DATEHOUR=$(date +"%d-%m-%Y_%H-%M-%S")
   FILE=backup-$POSTGRES_DATABASE-$DATEHOUR
 
-  DAY_BACKUP=$(aws --endpoint-url $S3_DESTINATION_HOST s3 ls s3://$S3_DESTINATION_BUCKET/postgres-$DATE.done)
-  # echo $DAY_BACKUP
-  if [ -n "$DAY_BACKUP" ]; then
-    echo "Backup already exist. Exit..."
-    exit 0
-  fi
-
-  set -e
+  # DAY_BACKUP=$(aws --endpoint-url $S3_DESTINATION_HOST s3 ls s3://$S3_DESTINATION_BUCKET/postgres-$DATE.done)
+  # # echo $DAY_BACKUP
+  # if [ -n "$DAY_BACKUP" ]; then
+  #   echo "Backup already exist. Exit..."
+  #   exit 0
+  # fi
 
   # if [ "$COMPRESSION_ENABLE" = "true" ]; then
   #   echo "Enable compression"
@@ -253,14 +249,27 @@ function backupAllPostgresToBucket() {
   echo "Begin Backup..."
   DATE_BEGIN=`date +%s`
 
-  PGPASSWORD=$POSTGRES_PASSWD pg_dumpall -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER | \
+  PGPASSWORD=$POSTGRES_PASSWD pg_dumpall -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER 2> dump_error.log | \
   aws --endpoint-url $S3_DESTINATION_HOST s3 cp - s3://$S3_DESTINATION_BUCKET/postgres-$DATE/$FILE.sql
+
+  if [[ -s "dump_error.log" ]]; then
+    cat dump_error.log
+    exit 6
+  fi
 
   DATE_ENDING=`date +%s`
   echo "Backup Done"
 
   SIZE=$(aws --endpoint-url $S3_DESTINATION_HOST s3 ls --summarize --human-readable s3://$S3_DESTINATION_BUCKET/postgres-$DATE/$FILE.sql | grep "Total Size" | awk -F': ' '{print $2}')
+  if [[ ! $SIZE =~ ^[0-9]+(\.[0-9]+)?[[:space:]][K|M|G]iB$ ]]; then
+    echo "Can't get backup Size from S3 ($SIZE)"
+    exit 2
+  fi
   TIME=$(secs_to_human $DATE_ENDING $DATE_BEGIN)
+  if [[ ! $TIME =~ ^[0-9]+h[[:space:]][0-9]{1,2}m[[:space:]][0-9]{1,2}s$ ]]; then
+    echo "Error with Time Calcul ($TIME)"
+    exit 3
+  fi
 
   echo "Resume:"
   echo "  File name: postgres-$DATE/$FILE.sql"
@@ -276,12 +285,21 @@ function backupAllPostgresToBucket() {
   rm postgres-$DATE.done
 
   LAST_BACKUP=$(check_last_backup "postgres" "postgres-$DATE.done")
-  # echo "Last Backup: $LAST_BACKUP.done"
+  if [[ ! $LAST_BACKUP =~ ^postgres-[0-9]{2}-[0-9]{2}-[0-9]{4}$ ]]; then
+    echo "Can't get last backup name from S3 ($LAST_BACKUP)"
+    exit 4
+  fi
+  echo "Last Backup: $LAST_BACKUP.done"
   LAST_SIZE_BACKUP=$(aws --endpoint-url $S3_DESTINATION_HOST s3 cp s3://$S3_DESTINATION_BUCKET/$LAST_BACKUP.done - | grep "Dump size:" | cut -d':' -f2)
-  # echo "Last Backup Size: $LAST_SIZE_BACKUP"
+  if [[ ! $LAST_SIZE_BACKUP =~ ^[[:space:]]*[0-9]+(\.[0-9]+)?[[:space:]][K|M|G]iB$ ]]; then
+    echo "Can't get last backup Size from S3 ($LAST_SIZE_BACKUP)"
+    exit 5
+  fi
+  echo "Last Backup Size: $LAST_SIZE_BACKUP"
 
   DIFF=$(compare_dump_size $SIZE $LAST_SIZE_BACKUP)
-  # echo $DIFF%
+  # [[ ! $DIFF =~ ^$ ]] && echo "Something wrong with diff calcul"; exit 6
+  echo "Difference since last backup: $DIFF%"
 
   if [ $DIFF -lt -5 ] || [ $DIFF -gt 5 ]; then
     echo "Difference too big: $DIFF%"
@@ -289,6 +307,12 @@ function backupAllPostgresToBucket() {
   fi
 
   echo "Backup checked"
+
+  set +e
+  echo "Remove old folder"
+  DATE=$(date -d "$RETENTION days ago" +"%d-%m-%Y")
+  aws --endpoint-url $S3_DESTINATION_HOST s3 rm --recursive s3://$S3_DESTINATION_BUCKET/postgres-$DATE
+
   exit 0
 }
 
